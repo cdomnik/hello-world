dbe76f124c73d7003c029be94e999fca1e4fd94d
cdomnik <30650575+cdomnik@users.noreply.github.com>
2017-08-02 11:25:34 +0200

    Created helloworld.c
    First Version of the hello-world code.

---
+++ helloworld.c
@@ -0,0 +1,13 @@
+/**********************************************************
+**  Simple Hello World Programm
+**  Nothing more to see here!!
+**  author: cdomnik
+**  version: 1, date: 02-oct-2017
+**********************************************************/
+#include<stdio.h>
+
+int main(void)
+{
+  printf("Hello World");
+  return 0;
+}
